19a5a2280eda2f0cbac8ff6dd04bb514775eaeca
stefaniemeliss <s_meliss@outlook.com>
2021-04-03 13:41:04 +0100

    new file doing concat for rest and task in one script

---
+++ fmri/bold/concat/run_3dTcat_after_preprocessing.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+################################################################################
+# concatenating task BOLD series after pre-processing
+################################################################################
+
+# load in AFNI module
+module load afni19.3.03
+
+# define path
+DIR="/storage/shared/research/cinn/2018/MAGMOT"
+
+# define derivatves dir
+deriv_dir="$DIR"/derivatives
+
+# change directory to BIDS folder
+BIDS_dir="$DIR"/MAGMOT_BIDS
+# change directory to the raw NIFI files
+cd $BIDS_dir
+
+# define subjects based on folder names in the BIDS directory
+subjects=($(ls -d sub*))
+#subjects=(sub-control001 sub-control002 sub-control003 sub-experimental004 sub-experimental005 sub-experimental006)
+#subjects=(sub-control003)
+#subjects=(sub-experimental016 sub-control045)
+subjects=(sub-experimental024)
+
+# define TSV file to read in
+input=$DIR/"scripts/fmri/bold/concat/MMC_inputForConcatenation.tsv"
+
+# define search and replace string for file prefix
+searchstring="desc-fullpreproc_bold.nii.gz"
+replacestring="desc-concat_bold.nii.gz"
+
+replacestring1="run-1_desc-fullpreproc_bold.nii.gz"
+replacestring2="run-2_desc-fullpreproc_bold.nii.gz"
+
+#searchstring="nosmooth_bold.nii.gz"
+#replacestring="nosmoothconcat_bold.nii.gz"
+
+#searchstring="smoothed_bold.nii.gz"
+#replacestring="smoothedconcat_bold.nii.gz"
+
+#searchstring="smoothed_bold.nii.gz"
+#replacestring="smoothedconcat_bold.nii.gz"
+
+# for each subject in the subjects array
+for subject in "${subjects[@]}"; do
+
+	echo $subject
+
+	# define concat directory
+	outdir=$deriv_dir/$subject/func
+
+	# load in pre-processed task file
+	cd $outdir
+
+    ############### concatenate TASK files ###############
+
+    # define task
+    task=magictrickwatching
+
+	# define niifile
+	taskfile="$subject"_task-"$task"_"$searchstring"
+
+	# set variable to track appearance of first magic trick
+	i=0
+	first=1
+
+	# read in file with information about scan duration
+	{
+	IGNORE_FL=T
+	while read ID stim_file start_vol end_vol
+		do
+
+		# if the line is header set IGNORE_FL to F and skip line
+		if [[ ${ID} = "ID" ]]; then
+			IGNORE_FL=F
+				continue
+		fi
+
+		# Ignore all lines until actual columns are found
+		if [[ ${IGNORE_FL} = T  ]]; then
+			continue
+		fi
+
+		# when the right information are available for the subject
+		if [[ "$ID" == *"$subject"* ]]; then
+
+			# update variable that tracks appearance of magictricks
+			((i=i+1))
+
+			# when we're dealing with the first magic trick
+			if [[ $i -eq $first ]]; then
+
+				echo "processing concatenation"
+				# determine start and end vol for the first magic trick and save them in select
+				select=`echo $start_vol..$end_vol`
+
+			else
+
+				# determine start and end vol for the next magic trick and save them in select_temp
+				select_temp=`echo $start_vol..$end_vol`
+				#update select to include start and end of all magic tricks processed so far
+				select=`echo $select,$select_temp`
+
+			fi
+
+
+		fi
+
+		done < "$input"
+		}
+
+	# once input file is processed
+	echo "selected vols: $select"
+
+	# 3dTcat to concatenate data using selected volumes
+	prefix="${taskfile/$searchstring/$replacestring}"
+	3dTcat $taskfile[$select] -prefix $prefix -session $outdir
+
+    # extract final file length
+    3dinfo $prefix > info.txt
+    numvol=$(awk 'NR==18 {print $6}' info.txt)
+    echo "final number of vols: $numvol"
+    rm info.txt
+
+    ############### concatenate REST files ###############
+
+    # define task
+    task=rest
+
+	# define niifile
+	restfile="$subject"_task-"$task"_"$searchstring"
+
+	# define tcat prefix
+	prefix1="${restfile/$searchstring/$replacestring1}"
+	prefix2="${restfile/$searchstring/$replacestring2}"
+
+	# create run-1 (first 300 vols)
+	if [ ! -f "$prefix1" ]; then
+		echo $prefix1
+		3dTcat -prefix $prefix1 $restfile[0..299]
+	fi
+
+	# create run-2 (second 300 vols)
+	if [ ! -f "$prefix2" ]; then
+		echo $prefix2
+		3dTcat -prefix $prefix2 $restfile[300..599]
+	fi
+
+done
